c1cef8dd4baa58e423132f5bc05f45d5beafaab5
Jeroen Kroese <jeroen@pix.nl>
2024-03-13 16:27:02 +0100

    Bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+	IonContent,
+	IonButton,
+	IonModal,
+	IonButtons,
+	IonItem,
+	IonInput,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+	standalone: true,
+	imports: [
+		IonInput,
+		IonItem,
+		IonButtons,
+		IonModal,
+		IonButton,
+		IonHeader,
+		IonToolbar,
+		IonTitle,
+		IonContent,
+	],
 })
 export class HomePage {
-  constructor() {}
+	constructor() {}
 }
